3c62c998ba7a41c1cbfe80b5774d55e6cbac86e9
Mandalus <malo.de-dinechin@epitech.eu>
2021-11-01 13:36:00 +0100

    feat(map movement): add map with movement restrictions
    Players can now move on the map
    Moving costs movement points
    If the player doesn't have enough points, he cannot move

--- Deckforce/Assets/Scripts/Player/MovePlayer.cs
+++ Deckforce/Assets/Scripts/Player/MovePlayer.cs
@@ -6,23 +6,29 @@ public class MovePlayer : MonoBehaviour
 {
     public SelectCase select;
 
-    private Transform character;
+    private Character character;
 
     // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<Transform>();
+        character = GetComponent<Character>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (select.currentSelected != null & Input.GetMouseButtonDown(0))
-        {
+        if (select.currentSelected != null && Input.GetMouseButtonDown(0) && character.canMove) {
             Vector3 casePos = select.currentSelected.GetComponentInParent<Transform>().position;
+            int distance = (int)Mathf.Abs((casePos - character.transform.position).magnitude);
             Debug.Log(casePos);
-            casePos.y += 0.5f;
-            character.position = casePos;
+            Debug.Log(distance);
+            int movementCost = 1 + distance/2;
+
+            if (character.currentMovePoints >= movementCost) {
+                casePos.y += 0.5f;
+                character.transform.position = casePos;
+                character.currentMovePoints -= movementCost;
+            }
         }
     }
 }
