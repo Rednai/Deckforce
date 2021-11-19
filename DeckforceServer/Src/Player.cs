5ee02a82d8dac0ee99745aa277fa2dd5c67bfa84
Hugo Boichard <71222348+HugoBoichard@users.noreply.github.com>
2021-11-19 23:31:59 +0900

    Recommit server with gitignore

---
+++ DeckforceServer/Src/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeckforceServer
+{
+    class Player
+    {
+        public string tag = Guid.NewGuid().ToString();
+        public int connectionId = 0;
+        public string ip = "";
+        public bool isConnected = false;
+    }
+}
---
+++ DeckforceServer/Src/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Playfab.Gaming.GSDK.CSharp;
+
+namespace DeckforceServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Api call to alert Playfab that the server is starting
+            GameserverSDK.Start();
+
+            Server server = new Server();
+            server.Start();
+        }
+    }
+}
