c3ca053295ddfc84f9c8f8d1b716657ce5c1369a
Dorian you <dorian.you@epitech.eu>
2021-12-15 22:15:59 +0900

    Bomb

---
+++ Deckforce/Assets/Scripts/Entities/Bomb.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : Entity
+{
+    public int damage;
+    public int sizeEffectRange;
+    public RangeType effectRange;
+    public AudioClip explosion;
+    public bool blockByEntity;
+
+    private Pathfinding pathfinding;
+    private Range range;
+    private BattleManager battleManager;
+
+    public override void Init()
+    {
+        pathfinding = GetComponent<Pathfinding>();
+        range = GetComponent<Range>();
+        battleManager = FindObjectOfType<BattleManager>();
+        base.Init();
+    }
+
+    public override void StartTurn()
+    {
+        base.StartTurn();
+        List<Tile> r = range.GetRangeTiles(pathfinding.startTile, effectRange, sizeEffectRange, true, blockByEntity);
+        foreach (Tile elem in r)
+            if (elem.tileEntity != null)
+                elem.tileEntity.TakeDamage(damage);
+        EndTurn();
+    }
+
+    public override void EndTurn()
+    {
+
+        base.EndTurn();
+        battleManager.FinishTurn();
+        Die();
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
+}
